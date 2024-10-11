d68dc1b76ad692b5e6ac999cc135db7a65dc6d48
somenko <somenko@sveak.com>
2024-10-11 12:39:56 +0300

    improvements

--- cmd/wp_comparison/files.go
+++ cmd/wp_comparison/files.go
@@ -10,26 +10,28 @@ import (
 	"path"
 	"strconv"
 	"time"
-	"unicode"
 )
 
-func generateFiles(base string) {
-	generateUrls(path.Join(base, "urls_10.txt"), 10, true)
-	generateUrls(path.Join(base, "urls_100.txt"), 100, true)
-	generateUrls(path.Join(base, "urls_1_000.txt"), 1_000, true)
-	generateUrls(path.Join(base, "urls_10_000.txt"), 10_000, true)
-	generateUrls(path.Join(base, "urls_100_000.txt"), 100_000, true)
-	generateUrls(path.Join(base, "urls_1_000_000.txt"), 1_000_000, true)
-	generateUrls(path.Join(base, "urls_10_000_000.txt"), 10_000_000, true)
+func generateFiles(folder, baseUrl string, counts []int) ([]string, error) {
+	files := make([]string, len(counts))
+	for idx, count := range counts {
+		generatedFile, err := generateUrls(fileName(folder, count), baseUrl, count, true)
+		if err != nil {
+			return nil, err
+		}
+		files[idx] = generatedFile
+	}
+
+	return files, nil
 }
 
-func generateUrls(file string, count int, showResults bool) error {
-	urls := fakeUrls(count, "http://localhost:8087/status/", true)
+func generateUrls(file, baseUrl string, count int, showResults bool) (string, error) {
+	urls := fakeUrls(count, baseUrl, true)
 
 	now := time.Now()
 	f, err := os.Create(file)
 	if err != nil {
-		return fmt.Errorf("create file: %w", err)
+		return "", fmt.Errorf("create file: %w", err)
 	}
 	defer f.Close()
 
@@ -37,19 +39,19 @@ func generateUrls(file string, count int, showResults bool) error {
 	for _, u := range urls {
 		_, err = writer.WriteString(u + "\n")
 		if err != nil {
-			return fmt.Errorf("write to file: %w", err)
+			return "", fmt.Errorf("write to file: %w", err)
 		}
 	}
 
 	err = writer.Flush()
 	if err != nil {
-		return fmt.Errorf("flush writer: %w", err)
+		return "", fmt.Errorf("flush writer: %w", err)
 	}
 	if showResults {
 		log.Printf("file generated in %s\n", time.Since(now))
 	}
 
-	return nil
+	return file, nil
 }
 
 func readUrls(file string, showResults bool) []string {
@@ -63,22 +65,9 @@ func readUrls(file string, showResults bool) []string {
 
 	scanner := bufio.NewScanner(f)
 
-	var digits []rune
-	for _, char := range file {
-		if unicode.IsDigit(char) {
-			digits = append(digits, char)
-		}
-	}
-
 	var lines []string
-	if len(digits) != 0 {
-		count, _ := strconv.Atoi(string(digits))
-		lines = make([]string, count)
-	}
-
-	for idx := 0; idx < len(lines); idx++ {
-		scanner.Scan()
-		lines[idx] = scanner.Text()
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -97,12 +86,23 @@ func fakeUrls(count int, baseUrl string, showResults bool) []string {
 	urls := make([]string, count)
 	for idx := 0; idx < len(urls); idx++ {
 		respCode := rand.Intn(500) + 200
-		path, _ := url.JoinPath(baseUrl, strconv.Itoa(respCode))
-		urls[idx] = path
+		p, _ := url.JoinPath(baseUrl, strconv.Itoa(respCode))
+		urls[idx] = p
 	}
 	if showResults {
 		log.Printf("fake url generated in %s\n", time.Since(start))
 	}
 
 	return urls
+}
+
+func fileName(folder string, count int) string {
+	var file string
+	for count > 1000 {
+		file += "k"
+		count /= 1000
+	}
+	file = "urls_" + strconv.Itoa(count) + file + ".txt"
+
+	return path.Join(folder, file)
 }
--- cmd/wp_comparison/main.go
+++ cmd/wp_comparison/main.go
@@ -1,49 +1,104 @@
+// Tasks:
+// 1. Make a function that runs goroutine for each http request
+// 2. Make same functionality but with worker pool
+// 3. Benchmark results against stubs and real http request
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"runtime"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
-// TODO сделать на воркер пуле с реальными урлами
-
-const (
-	file_10         = "/tmp/urls_10.txt"
-	file_100        = "/tmp/urls_100.txt"
-	file_1_000      = "/tmp/urls_1_000.txt"
-	file_10_000     = "/tmp/urls_10_000.txt"
-	file_100_000    = "/tmp/urls_100_000.txt"
-	file_1_000_000  = "/tmp/urls_1_000_000.txt"
-	file_10_000_000 = "/tmp/urls_10_000_000.txt"
-)
-
 func main() {
-	fmt.Println(runtime.NumCPU())
+	genFiles := flag.Bool("g", false, "generate files with fake urls")
+	genPath := flag.String("p", "/tmp/", "path for generated files, used only with -g")
+	genBaseUrl := flag.String("u", "http://localhost:8087/status/", "base url for generated url, used only with -g")
+	genCounts := flag.String("c", "10,100,1000,10000", "number of urls to generate, separated by comma for each file")
+	testFile := flag.String("f", "/tmp/urls_10k.txt", "file with urls to test")
+	flag.Parse()
 
-	urls := readUrls(file_10_000, true)
+	if *genFiles {
+		c := strings.Split(*genCounts, ",")
+		var counts []int
+		for _, s := range c {
+			atoi, err := strconv.Atoi(s)
+			if err != nil {
+				log.Fatalf("failed to parse count: %v\n", err)
+			}
+			counts = append(counts, atoi)
+		}
 
-	goEachRequestStub(urls, true)
-	goEachRequestReal(urls, true)
-	workerPoolStub(urls, 2, true)
-	workerPoolStub(urls, 4, true)
-	workerPoolStub(urls, 8, true)
-	workerPoolStub(urls, 16, true)
-	workerPoolStub(urls, 32, true)
-	workerPoolReal(urls, 2, true)
-	workerPoolReal(urls, 4, true)
-	workerPoolReal(urls, 8, true)
-	workerPoolReal(urls, 16, true)
-	workerPoolReal(urls, 32, true)
-	workerPoolStub(urls, 256, true)
-	workerPoolReal(urls, 256, true)
+		files, err := generateFiles(*genPath, *genBaseUrl, counts)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("files generated: %v\n", files)
+	}
+
+	urls := readUrls(*testFile, true)
+
+	fmt.Printf(`### INFO
+Processors:		%d
+File:			%s
+Lines in file:		%d
+###
+`, runtime.NumCPU(), *testFile, len(urls))
+
+	stub := func(url string) int {
+		return 200
+	}
+	realRequest := func(url string) int {
+		resp, err := http.Get(url)
+		if err != nil {
+			return -1
+		}
+		return resp.StatusCode
+	}
+
+	fmt.Println("stubs")
+	goEachRequest(urls, stub, true)
+	workerPool(urls, 1, stub, true)
+	workerPool(urls, 2, stub, true)
+	workerPool(urls, 4, stub, true)
+	workerPool(urls, 8, stub, true)
+	workerPool(urls, 16, stub, true)
+	workerPool(urls, 32, stub, true)
+	workerPool(urls, 256, stub, true)
+	workerPool2(urls, 1, stub, true)
+	workerPool2(urls, 2, stub, true)
+	workerPool2(urls, 4, stub, true)
+	workerPool2(urls, 8, stub, true)
+	workerPool2(urls, 16, stub, true)
+	workerPool2(urls, 32, stub, true)
+	workerPool2(urls, 256, stub, true)
+
+	fmt.Printf("\nreal requests\n")
+	goEachRequest(urls, realRequest, true)
+	workerPool(urls, 1, realRequest, true)
+	workerPool(urls, 2, realRequest, true)
+	workerPool(urls, 4, realRequest, true)
+	workerPool(urls, 8, realRequest, true)
+	workerPool(urls, 16, realRequest, true)
+	workerPool(urls, 32, realRequest, true)
+	workerPool(urls, 256, realRequest, true)
+	workerPool2(urls, 1, realRequest, true)
+	workerPool2(urls, 2, realRequest, true)
+	workerPool2(urls, 4, realRequest, true)
+	workerPool2(urls, 8, realRequest, true)
+	workerPool2(urls, 16, realRequest, true)
+	workerPool2(urls, 32, realRequest, true)
+	workerPool2(urls, 256, realRequest, true)
 }
 
-type stats map[int]int
-
-func goEachRequestStub(urls []string, showResults bool) stats {
+// goEachRequest creates a goroutine for each element of urls to get response code.
+func goEachRequest(urls []string, reqFn func(url string) int, showResults bool) map[int]int {
 	start := time.Now()
 	result := make(map[int]int)
 	mu := sync.Mutex{}
@@ -51,51 +106,27 @@ func goEachRequestStub(urls []string, showResults bool) stats {
 
 	for _, u := range urls {
 		wg.Add(1)
-		go func() {
+		go func(url string) {
 			defer wg.Done()
-			code := requestStub(u)
+			code := reqFn(url)
 
 			mu.Lock()
 			result[code]++
 			mu.Unlock()
-		}()
+		}(u)
 	}
 	wg.Wait()
 	if showResults {
-		//log.Printf("stats done in %s\n", time.Since(start))
-		fmt.Printf("%s\n", time.Since(start))
+		bench(1, start)
 	}
 
 	return result
 }
 
-func goEachRequestReal(urls []string, showResults bool) stats {
-	start := time.Now()
-	result := make(map[int]int)
-	mu := sync.Mutex{}
-	var wg sync.WaitGroup
-
-	for _, u := range urls {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			code := doRequest(u)
-
-			mu.Lock()
-			result[code]++
-			mu.Unlock()
-		}()
-	}
-	wg.Wait()
-	if showResults {
-		fmt.Printf("%s\n", time.Since(start))
-		//log.Printf("stats done in %s\n", time.Since(start))
-	}
-
-	return result
-}
-
-func workerPoolStub(urls []string, workerCount int, showResults bool) stats {
+// workerPool makes requests using worker pool:
+// - Result calculated inside each worker
+// - Map access synced with mutex
+func workerPool(urls []string, workerCount int, reqFn func(url string) int, showResults bool) map[int]int {
 	start := time.Now()
 	result := make(map[int]int)
 	mu := sync.Mutex{}
@@ -108,7 +139,7 @@ func workerPoolStub(urls []string, workerCount int, showResults bool) stats {
 		go func(jobs chan string) {
 			defer wg.Done()
 			for job := range jobs {
-				code := requestStub(job)
+				code := reqFn(job)
 
 				mu.Lock()
 				result[code]++
@@ -125,31 +156,28 @@ func workerPoolStub(urls []string, workerCount int, showResults bool) stats {
 
 	wg.Wait()
 	if showResults {
-		fmt.Printf("%s\n", time.Since(start))
-		//log.Printf("stats done in %s\n", time.Since(start))
+		benchWorker(1, start, workerCount)
 	}
 
 	return result
 }
 
-func workerPoolReal(urls []string, workerCount int, showResults bool) stats {
+// workerPool2 makes requests using worker pool too, but:
+// - Jobs channel is larger
+// - Result calculated by 'results' channel
+// - Map access sync is not required
+func workerPool2(urls []string, workerCount int, reqFn func(url string) int, showResults bool) map[int]int {
 	start := time.Now()
 	result := make(map[int]int)
-	mu := sync.Mutex{}
-	wg := sync.WaitGroup{}
+	jobsCount := len(urls)
 	jobs := make(chan string, workerCount)
+	results := make(chan int, jobsCount)
 
 	// run workers
 	for w := 1; w <= workerCount; w++ {
-		wg.Add(1)
 		go func(jobs chan string) {
-			defer wg.Done()
 			for job := range jobs {
-				code := doRequest(job)
-
-				mu.Lock()
-				result[code]++
-				mu.Unlock()
+				results <- reqFn(job)
 			}
 		}(jobs)
 	}
@@ -160,24 +188,24 @@ func workerPoolReal(urls []string, workerCount int, showResults bool) stats {
 	}
 	close(jobs)
 
-	wg.Wait()
+	// get results
+	for i := 0; i < jobsCount; i++ {
+		result[<-results]++
+	}
+
 	if showResults {
-		fmt.Printf("%s\n", time.Since(start))
-		//log.Printf("stats done in %s\n", time.Since(start))
+		benchWorker(1, start, workerCount)
 	}
 
 	return result
 }
 
-func requestStub(url string) int {
-	return 200
+func bench(skip int, start time.Time) {
+	pc, _, _, _ := runtime.Caller(skip)
+	fmt.Printf("%s:\t\t%s\n", runtime.FuncForPC(pc).Name(), time.Since(start))
 }
 
-func doRequest(url string) int {
-	resp, err := http.Get(url)
-	if err != nil {
-		//log.Printf("http.Get error: %v\n", err)
-		return -1
-	}
-	return resp.StatusCode
+func benchWorker(skip int, start time.Time, workerCount int) {
+	pc, _, _, _ := runtime.Caller(skip)
+	fmt.Printf("%s [%d]:\t\t%s\n", runtime.FuncForPC(pc).Name(), workerCount, time.Since(start))
 }
